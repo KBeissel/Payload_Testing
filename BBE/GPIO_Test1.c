d4f839526b723cde8672fc101c245a12cf85b184
KBeissel <kyle.beissel@ndus.edu>
2021-01-27 23:15:28 -0500

    Commented out system function. Causing error will need to be resolved.

--- BBE/GPIO_Test1.c
+++ BBE/GPIO_Test1.c
@@ -7,8 +7,8 @@
 #include <fcntl.h>
 
 int main() {
-	system("echo 49 > /sys/class/gpio/export");
-	system("echo out > /sys/class/gpio/gpio49/direction");
+	//system("echo 49 > /sys/class/gpio/export");
+	//system("echo out > /sys/class/gpio/gpio49/direction");
 	int fd = open("/sys/class/gpio/gpio/value", O_WRONLY);
 	int i = 0;
 	while (i < 50) {
